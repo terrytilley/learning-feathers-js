ced519a4d4364efd1846e7287574e5349f4304f7
Terry <terry@terrytilley.com>
2016-12-20 16:27:34 +0000

    init commit

---
+++ src/services/message/hooks/index.js
@@ -0,0 +1,37 @@
+'use strict';
+
+const restrictToSender = require('./restrict-to-sender');
+
+const auth = require('feathers-authentication').hooks;
+const hooks = require('feathers-hooks');
+
+const process = require('./process');
+const globalHooks = require('../../../hooks');
+const populateSender = hooks.populate('sentBy', {
+  service: 'users',
+  field: 'userId'
+});
+
+exports.before = {
+  all: [
+    auth.verifyToken(),
+    auth.populateUser(),
+    auth.restrictToAuthenticated()
+  ],
+  find: [],
+  get: [],
+  create: [process(), restrictToSender()],
+  update: [hooks.remove('sentBy')],
+  patch: [hooks.remove('sentBy')],
+  remove: []
+};
+
+exports.after = {
+  all: [],
+  find: [populateSender],
+  get: [populateSender],
+  create: [populateSender],
+  update: [],
+  patch: [],
+  remove: []
+};
